dee633bb0a57cf28c9b6634d258973749947f7de
Mohammed <msk17a@gmail.com>
2023-02-26 21:25:53 +0300

    Modified

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,1 +1,1 @@
-curl https://learn.reboot01.com/assets/superhero/all.json | jq '.[] | select( .id == 20 ).name'
+curl https://learn.reboot01.com/assets/superhero/all.json | jq '.[] | select( .id == 70 ).name'
