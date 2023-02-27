73ba664ca499b4e587aad077ed9e665647c5a02a
MSK17A <legendary512@gmail.com>
2023-02-28 01:24:47 +0300

    .

---
+++ printcomb2.go
@@ -0,0 +1,26 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for x := '0'; x <= '9'; x++ {
+				for y := '0'; y <= '9'; y++ {
+
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(x)
+					z01.PrintRune(y)
+					if i == '9' && j == '9' && x == '9' && y == '9' {
+						break
+					}
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
