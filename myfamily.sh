67f073e6a1f644c0fe45b0db36c8d568894998e5
MSK17A <legendary512@gmail.com>
2023-02-27 13:14:23 +0300

    .

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,2 @@
-curl https://learn.reboot01.com/assets/superhero/all.json | jq '.[] | select( .id == 70 ).name'
+VARIABLE_ID=$(printenv HERO_ID)
+curl https://learn.reboot01.com/assets/superhero/all.json | jq '.[] | select( .id == '${VARIABLE_ID}' ).connections.relatives' | tr -d '"'
