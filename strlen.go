0f29d4bfecc9d5a22781cb5fb81bab2698430585
MSK17A <legendary512@gmail.com>
2023-02-28 18:39:36 +0300

    1

--- strlen.go
+++ strlen.go
@@ -1,18 +1,12 @@
 package piscine
 
-import (
-	"fmt"
-)
-
 func StrLen(s string) int {
 	runes_counter := 0
 
 	for i := range s {
 		if s[i] > 0 {
-
 			runes_counter++
 		}
-		fmt.Println(s[i])
 	}
 	return runes_counter
 }
