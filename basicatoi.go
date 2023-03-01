ae0aaeffcc834aca011da813a21f6510ff2c0998
MSK17A <legendary512@gmail.com>
2023-03-01 23:02:48 +0300

    .

--- basicatoi.go
+++ basicatoi.go
@@ -1,6 +1,15 @@
 package piscine
 
 func BasicAtoi(s string) int {
+	num := 0
+
+	for i := 0; i < len(s); i++ {
+		num = num*10 + (int(s[i]) - 48)
+	}
+	return num
+}
+
+/*func BasicAtoi(s string) int {
 	_s := []byte(s)
 	count := 0
 	for range _s {
@@ -13,4 +22,4 @@ func BasicAtoi(s string) int {
 		pow *= 10
 	}
 	return result
-}
+}*/
