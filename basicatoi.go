b24db234c1016a77e7d4547fa18ded965ef49a16
malsamma <msk170a@gmail.com>
2023-03-01 17:47:45 +0000

    .

--- basicatoi.go
+++ basicatoi.go
@@ -1,9 +1,10 @@
 package piscine
 
 func BasicAtoi(s string) int {
-	if s == 0 {
-		return
+	num := 0
+	num = int(s[0]-48)*10 + (int(s[1]) - 48)
+	for i := 1; i < (len(s) - 1); i++ {
+		num = num*10 + (int(s[i+1]) - 48)
 	}
-	BasicAtoi(s / 10)
-	num = s*10 + s%10
+	return num
 }
--- main/main.go
+++ main/main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"fmt"
+	"piscine"
 )
 
-func BasicAtoi(s string) int {
-	num := 0
-	num = int(s[0]-48)*10 + (int(s[1]) - 48)
-	for i := 1; i < (len(s) - 1); i++ {
-		num = num*10 + (int(s[i+1]) - 48)
-	}
-	return num
-}
-
 func main() {
-	fmt.Println(BasicAtoi("12345"))
-	fmt.Println(BasicAtoi("0000000012345"))
-	fmt.Println(BasicAtoi("000000"))
+	fmt.Println(piscine.BasicAtoi("12345"))
+	fmt.Println(piscine.BasicAtoi("0000000012345"))
+	fmt.Println(piscine.BasicAtoi("000000"))
 }
