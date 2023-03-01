71930fa1f89278934801494ef07d68cecf19238b
malsamma <msk170a@gmail.com>
2023-03-01 19:09:18 +0000

    .

--- basicatoi.go
+++ basicatoi.go
@@ -2,13 +2,11 @@ package piscine
 
 func BasicAtoi(s string) int {
 	num := 0
-	if StrLen(s) < 3 {
-		num = int(s[0] - 48)
-		return num
-	}
-	num = int(s[0]-48)*10 + (int(s[1]) - 48)
-	for i := 1; i < (StrLen(s) - 1); i++ {
-		num = num*10 + (int(s[i+1]) - 48)
+	
+	for _, word := range s {
+		for i:='0'; i<word; i++ {
+			
+		}
 	}
 	return num
 }
