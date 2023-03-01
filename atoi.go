2763510d060019a76367129909f8bc4229c380e8
MSK17A <legendary512@gmail.com>
2023-03-02 00:28:08 +0300

    l

--- atoi.go
+++ atoi.go
@@ -4,11 +4,12 @@ func Atoi(s string) int {
 	num := 0
 	sign := 1
 	sign_count := 0
+	string_length := len(s)
 
-	for i := 0; i < len(s); i++ {
+	for i := 0; i < string_length; i++ {
 		if s[i] == 45 || s[i] == 43 {
 			sign_count += 1
-		} else if ((s[i] < 48) || (s[i] > 57)) && (s[0] != 45 && s[0] != 43) {
+		} else if ((s[i] < 48) || (s[i] > 57)) && (s[i] != 45 && s[i] != 43) {
 			return 0
 		}
 		if sign_count > 1 {
@@ -17,16 +18,16 @@ func Atoi(s string) int {
 	}
 	if s[0] == 45 {
 		sign = -1
-		for i := 1; i < len(s); i++ {
+		for i := 1; i < string_length; i++ {
 			num = num*10 + (int(s[i]) - 48)
 		}
 	} else if s[0] == 43 {
 		sign = 1
-		for i := 1; i < len(s); i++ {
+		for i := 1; i < string_length; i++ {
 			num = num*10 + (int(s[i]) - 48)
 		}
 	} else {
-		for i := 0; i < len(s); i++ {
+		for i := 0; i < string_length; i++ {
 			num = num*10 + (int(s[i]) - 48)
 		}
 	}
