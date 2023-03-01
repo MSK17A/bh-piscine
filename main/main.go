4f72c6a833cbea935620bdd4678175a70bd50b06
MSK17A <legendary512@gmail.com>
2023-03-01 07:38:28 +0300

    .

--- main/main.go
+++ main/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "piscine"
+import (
+	"github.com/01-edu/z01"
+	"piscine"
+)
 
 func main() {
-	piscine.PrintComb2()
+	piscine.PrintNbr(-123)
+	piscine.PrintNbr(0)
+	piscine.PrintNbr(123)
+	z01.PrintRune('\n')
 }
---
+++ printnbr.go
@@ -0,0 +1,27 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func conv_to_ASCII(num, sign int) {
+	if num == 0 {
+		return
+	}
+
+	digit := int(num % 10 * sign)
+	_num := int(num / 10)
+	conv_to_ASCII(_num, sign) // Recursive
+	z01.PrintRune(48 + rune(digit))
+}
+func PrintNbr(n int) {
+	sign := 1
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+
+	if n < 0 {
+		sign *= -1
+		z01.PrintRune('-')
+	}
+	conv_to_ASCII(n, sign)
+}
