9464de99298b50eda06fb4752c8884fe82d4f7dc
MSK17A <legendary512@gmail.com>
2023-03-02 00:56:52 +0300

    ;

--- atoi.go
+++ atoi.go
@@ -1,37 +1,36 @@
 package piscine
 
 func Atoi(s string) int {
-	_s := []byte(s)
 	num := 0
 	sign := 1
 	sign_count := 0
 	string_length := len(s)
 
 	for i := 0; i < string_length; i++ {
-		if _s[i] == 45 || _s[i] == 43 {
+		if s[i] == 45 || s[i] == 43 {
 			sign_count += 1
-		} else if ((_s[i] < 48) || (_s[i] > 57)) && (_s[i] != 45 && _s[i] != 43) {
+		} else if ((s[i] < 48) || (s[i] > 57)) && (s[i] != 45 && s[i] != 43) {
 			return 0
 		}
 		if sign_count > 1 {
 			return 0
 		}
 	}
-	if _s[0] == 45 {
+	/*if s[0] == 45 {
 		sign = -1
 		for i := 1; i < string_length; i++ {
-			num = num*10 + (int(_s[i]) - 48)
+			num = num*10 + (int(s[i]) - 48)
 		}
-	} else if _s[0] == 43 {
+	} else if s[0] == 43 {
 		sign = 1
 		for i := 1; i < string_length; i++ {
-			num = num*10 + (int(_s[i]) - 48)
+			num = num*10 + (int(s[i]) - 48)
 		}
 	} else {
 		for i := 0; i < string_length; i++ {
-			num = num*10 + (int(_s[i]) - 48)
+			num = num*10 + (int(s[i]) - 48)
 		}
-	}
+	}*/
 
 	return num * sign
 }
