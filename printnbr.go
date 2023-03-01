bb6cabc3fefaad46edd41964538d080f171d0dd0
MSK17A <legendary512@gmail.com>
2023-03-01 07:50:10 +0300

    ;l

--- printnbr.go
+++ printnbr.go
@@ -2,15 +2,6 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func conv_to_ASCII(num int, sign int) {
-	if num == 0 {
-		return
-	}
-	digit := int(num % 10 * sign)
-	_num := int(num / 10)
-	conv_to_ASCII(_num, sign)
-	z01.PrintRune(48 + rune(digit))
-}
 func PrintNbr(n int) {
 	sign := 1
 	if n == 0 {
