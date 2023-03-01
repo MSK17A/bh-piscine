863667a33ba2d5a555d0ffa3efce4a30ec93cd27
MSK17A <legendary512@gmail.com>
2023-03-02 01:31:18 +0300

    .

--- atoi.go
+++ atoi.go
@@ -1,6 +1,6 @@
 package piscine
 
-func Atoi(s string) int {
+/*func Atoi(s string) int {
 	num := 0
 	sign := 1
 	sign_count := 0
@@ -32,7 +32,7 @@ func Atoi(s string) int {
 		}
 	}
 	return num * sign
-}
+}*/
 
 /*
 func Atoi(s string) int {
@@ -63,3 +63,25 @@ func Atoi(s string) int {
 	return result * sign
 }
 */
+func Atoi(s string) int {
+	num := 0
+	sign := 1
+	string_length := len(s)
+	index := 0
+
+	if s[0] == 45 {
+		sign = -1
+		index = 1
+	} else if s[0] == 43 {
+		sign = 1
+		index = 1
+	}
+
+	for i := index; i < string_length; i++ {
+		if (s[i] < 48) || (s[i] > 57) {
+			return 0
+		}
+		num = num*10 + (int(s[i]) - 48)
+	}
+	return num * sign
+}
