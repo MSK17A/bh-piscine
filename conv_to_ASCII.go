d52a433828b5ea686768b704ac648f51ea528592
malsamma <msk17a@gmail.com.com>
2023-03-01 08:17:26 +0300

    .

--- conv_to_ASCII.go
+++ conv_to_ASCII.go
@@ -6,7 +6,7 @@ func conv_to_ASCII(num int, sign int) {
 	if num == 0 {
 		return
 	}
-	digit := int(num % 10 * sign)
+	digit := int((num % 10) * sign)
 	_num := int(num / 10)
 	conv_to_ASCII(_num, sign)
 	z01.PrintRune(48 + rune(digit))
