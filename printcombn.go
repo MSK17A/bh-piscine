aec87a3910b5c76fc76061987689b9d16eded4b7
MSK17A <legendary512@gmail.com>
2023-03-03 00:14:34 +0300

    b

---
+++ printcombn.go
@@ -0,0 +1,21 @@
+package piscine
+
+import "fmt"
+
+func PrintCombN(n int) {
+	max_num:=0
+	zeros := 0
+	for i:=0; i<n; i++ {
+		max_num = max_num*10+9
+	}
+
+	for i:=0; i<max_num; i++ {
+		for j:=0; j<n; j++ {
+			if i/10 > 0 {
+				continue
+			}
+			else if i/10
+		}
+		fmt.Println(i)
+	}
+}
