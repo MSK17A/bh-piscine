df7145f019b7d814676b94564e4a239d0b1ace7d
malsamma <msk17a@gmail.com.com>
2023-03-04 11:54:50 +0300

    .

---
+++ isprime.go
@@ -0,0 +1,21 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	if nb < 2 {
+		return false
+	}
+
+	result := false
+
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			result = false
+			break
+		}
+		if i == nb-1 {
+			result = true
+		}
+	}
+
+	return result
+}
