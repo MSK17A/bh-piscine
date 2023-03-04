7800f140c446921e45d16288ffc79715d472f889
malsamma <msk17a@gmail.com.com>
2023-03-04 10:43:58 +0300

    /

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,8 +1,11 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 1 || nb > 69 {
+	if nb < 0 || nb > 69 {
 		return 0
+	}
+	if nb == 0 {
+		return 1
 	}
 	result := 1
 	for i := 1; i <= nb; i++ {
