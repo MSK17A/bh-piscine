69ff2cbc56429fa2ed059133c948a40ea3b87fec
Mohammed <msk170a@gmail.com>
2023-03-05 20:45:18 +0300

    .

--- index.go
+++ index.go
@@ -1,12 +1,27 @@
 package piscine
 
+func strLen(s string) int {
+	result := 0
+	for count, _ := range s {
+		result = count
+	}
+	return result
+}
+
 func Index(s string, toFind string) int {
 	if len(s) < 1 || len(toFind) < 1 {
 		return 0
 	}
-	for count, word := range s {
-		if word == rune(toFind[0]) {
-			return count
+	if s[0] == 92 {
+		return -1
+	}
+	var s_rune = []rune(s)
+	var f_rune = []rune(toFind)
+	s_rune_length := strLen(s)
+
+	for i := 0; i < s_rune_length; i++ {
+		if s_rune[i] == f_rune[0] {
+			return i
 		}
 	}
 	return -1
--- test/main.go
+++ test/main.go
@@ -9,4 +9,5 @@ func main() {
 	fmt.Println(piscine.Index("Hello!", "l"))
 	fmt.Println(piscine.Index("Salut!", "alu"))
 	fmt.Println(piscine.Index("Ola!", "hOl"))
+	fmt.Println(piscine.Index("\\~gahTCljM]f;", "aoxsA;<1t%5f$"))
 }
