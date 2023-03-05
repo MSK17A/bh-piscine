4e25f7fc05b860cb7f64c03a63919514c7e97b38
Mohammed <msk170a@gmail.com>
2023-03-05 21:51:49 +0300

    .

--- index.go
+++ index.go
@@ -12,16 +12,25 @@ func Index(s string, toFind string) int {
 	if len(s) < 1 || len(toFind) < 1 {
 		return 0
 	}
-	if s[0] == 92 {
-		return -1
-	}
+
 	s_rune := []rune(s)
 	f_rune := []rune(toFind)
 	s_rune_length := strLen(s)
+	f_rune_length := strLen(toFind)
+	Similarity_count := 0
 
-	for i := 0; i < s_rune_length; i++ {
+	for i := 0; i <= s_rune_length; i++ {
 		if s_rune[i] == f_rune[0] {
-			return i
+			for j := 0; j <= f_rune_length; j++ {
+				if i+j <= s_rune_length {
+					if s_rune[i+j] == f_rune[j] {
+						Similarity_count++
+					}
+					if Similarity_count >= f_rune_length {
+						return i
+					}
+				}
+			}
 		}
 	}
 	return -1
--- test/main.go
+++ test/main.go
@@ -11,4 +11,5 @@ func main() {
 	fmt.Println(piscine.Index("Ola!", "hOl"))
 	fmt.Println(piscine.Index("\\~gahTCljM]f;", "aoxsA;<1t%5f$"))
 	fmt.Println(piscine.Index("\\ti^&9o<_o-_.", "&9o"))
+	fmt.Println(piscine.Index("J(1cTJj[>Tdtn", "(DWK-)E4<$&2)"))
 }
