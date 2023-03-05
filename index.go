726473e607b8427f4a1dde591175563aeb2319db
Mohammed <msk170a@gmail.com>
2023-03-05 19:57:11 +0300

    .

--- index.go
+++ index.go
@@ -2,7 +2,7 @@ package piscine
 
 func Index(s string, toFind string) int {
 	if len(s) < 1 || len(toFind) < 1 {
-		return -1
+		return 0
 	}
 	for count, word := range s {
 		if word == rune(toFind[0]) {
