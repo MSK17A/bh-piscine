38d6f2cb0db1464313961e675239115728f10117
MSK17A <legendary512@gmail.com>
2023-03-05 23:31:06 +0300

    .

---
+++ concat.go
@@ -0,0 +1,9 @@
+package piscine
+
+func Concat(str1 string, str2 string) string {
+
+	if str1 == "" || str2 == "" {
+		return "0"
+	}
+	return str1 + str2
+}
--- test/main.go
+++ test/main.go
@@ -6,10 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.Index("Hello!", "l"))
-	fmt.Println(piscine.Index("Salut!", "alu"))
-	fmt.Println(piscine.Index("Ola!", "hOl"))
-	fmt.Println(piscine.Index("\\~gahTCljM]f;", "aoxsA;<1t%5f$"))
-	fmt.Println(piscine.Index("\\ti^&9o<_o-_.", "&9o"))
-	fmt.Println(piscine.Index("J(1cTJj[>Tdtn", "(DWK-)E4<$&2)"))
+	fmt.Println(piscine.Concat("Hello!", " How are you?"))
+
 }
