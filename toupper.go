215dec64abab4fa40d29686efe7691d890223e3a
malsamma <msk17a@gmail.com.com>
2023-03-05 14:49:39 +0300

    .

--- toupper.go
+++ toupper.go
@@ -2,4 +2,17 @@ package piscine
 
 func ToUpper(s string) string {
 
+	if s == "" {
+		return '0'
+	}
+
+	s_upper := []rune(s)
+	for counter, word := range s {
+		if word >= 97 && word <= 122 {
+			s_upper[counter] = word - 32
+		} else {
+			s_upper[counter] = word
+		}
+	}
+	return string(s_upper)
 }
