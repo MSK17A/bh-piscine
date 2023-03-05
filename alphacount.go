9ab1bc45340263f5c2b8fd8117e5dcc40489d1b5
malsamma <msk17a@gmail.com.com>
2023-03-05 11:43:27 +0300

    .

---
+++ alphacount.go
@@ -0,0 +1,16 @@
+package piscine
+
+func AlphaCount(s string) int {
+	if len(s) < 0 {
+		return 0
+	}
+	result := 0
+
+	for _, word := range s {
+		if (word >= 65 && word <= 90) || (word >= 92 && word <= 122) {
+			result++
+		}
+	}
+
+	return result
+}
