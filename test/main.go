0e2fd29c5cc508550d9e779523ecb46cd6003e34
malsamma <msk17a@gmail.com.com>
2023-03-05 13:55:43 +0300

    .

--- test/main.go
+++ test/main.go
@@ -6,15 +6,10 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
+	
 	fmt.Println(piscine.IsAlpha("Hello! How are you?"))
 	fmt.Println(piscine.IsAlpha("HelloHowareyou"))
 	fmt.Println(piscine.IsAlpha("What's this 4?"))
 	fmt.Println(piscine.IsAlpha("Whatsthis4"))
 
-=======
-	fmt.Println(piscine.IsLower("hello"))
-	fmt.Println(piscine.IsLower("HELLO!"))
-	fmt.Println(piscine.IsLower("eangshp"))
->>>>>>> refs/remotes/origin/master
 }
