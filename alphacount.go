b6ae705305da796bf7521636ec3ef8fd85fb70b1
malsamma <msk17a@gmail.com.com>
2023-03-05 11:48:16 +0300

    .

--- alphacount.go
+++ alphacount.go
@@ -7,7 +7,7 @@ func AlphaCount(s string) int {
 	result := 0
 
 	for _, word := range s {
-		if (word >= 65 && word <= 90) || (word >= 92 && word <= 122) {
+		if (word >= 65 && word <= 90) || (word >= 97 && word <= 122) {
 			result++
 		}
 	}
