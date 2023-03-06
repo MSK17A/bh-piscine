42d30936a65d91c87f958b0cbc6bd951e93add85
MSK17A <legendary512@gmail.com>
2023-03-06 22:50:14 +0300

    .

--- capitalize.go
+++ capitalize.go
@@ -1,38 +1,54 @@
 package piscine
 
-func runeIsAlpha(r rune) bool {
-	if (r < 65 || r > 90) && (r < 97 || r > 122) && (r < 48 || r > 57) {
-		return false
+func isCapital(word rune) bool {
+	if 'A' <= word && word <= 'Z' {
+		return true
 	}
-	return true
+	return false
 }
 
+func isNumber(word rune) bool {
+	if '0' <= word && word <= '9' {
+		return true
+	}
+	return false
+}
+
+func isLowerCase(word rune) bool {
+	if 'a' <= word && word <= 'z' {
+		return true
+	}
+	return false
+}
+
+func isAlphaNum(word rune) bool {
+	if isCapital(word) || isNumber(word) || isLowerCase(word) {
+		return true
+	}
+	return false
+}
 func Capitalize(s string) string {
 	// If the previous rune contains non alphanumerical characters CAPITALIZE the current rune
 	// Other wise lower the current rune
 	// If the next rune is not alphanumerical (don't do anything)
 	s_rune := []rune(s)
-	s_length := strLen(s)
+	capitalize_flag := true
 
-	if runeIsAlpha(s_rune[1]) {
-		// If it is lowercase then UPPERCASE IT
-		if s_rune[0] >= 97 && s_rune[0] <= 122 {
-			s_rune[0] -= 32
-		}
-	}
-
-	for i := 1; i <= s_length; i++ {
-		// if it is not alphanumerical (the previous rune)
-		if i < s_length {
-			if !runeIsAlpha(s_rune[i-1]) && runeIsAlpha(s_rune[i+1]) {
-				// If it is lowercase then UPPERCASE IT
-				if s_rune[i] >= 97 && s_rune[i] <= 122 {
-					s_rune[i] -= 32
-				}
+	for idx, word := range s_rune {
+		if capitalize_flag {
+			if isCapital(word) || isNumber(word) {
+				capitalize_flag = false
+				continue
+			} else if isLowerCase(word) {
+				s_rune[idx] -= 32
+				capitalize_flag = false
 			}
-		} else if s_rune[i] >= 65 && s_rune[i] <= 90 && runeIsAlpha(s_rune[i-1]) {
-			s_rune[i] += 32
+		} else if !capitalize_flag && isCapital(word) {
+			s_rune[idx] += 32
+		} else if !isAlphaNum(word) {
+			capitalize_flag = true
 		}
 	}
+
 	return string(s_rune)
 }
