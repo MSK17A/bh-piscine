f8e830396946afb6c8fdb3c4df8dad4c5a2776f7
MSK17A <legendary512@gmail.com>
2023-03-06 21:59:29 +0300

    .

--- capitalize.go
+++ capitalize.go
@@ -10,7 +10,6 @@ func Capitalize(s string) string {
 	// If the previous rune contains non alphanumerical characters CAPITALIZE the current rune
 	// Other wise lower the current rune
 	// If the next rune is not alphanumerical (don't do anything)
-
 	s_rune := []rune(s)
 	s_length := strLen(s)
 
