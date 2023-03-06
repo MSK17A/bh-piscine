24dd99788ef0ae01c7c97e8ededa54f9d93a9022
MSK17A <legendary512@gmail.com>
2023-03-06 23:07:28 +0300

    .

---
+++ basicjoin.go
@@ -0,0 +1,10 @@
+package piscine
+
+func BasicJoin(elems []string) string {
+	var str string
+
+	for i := 0; i < len(elems); i++ {
+		str += elems[i]
+	}
+	return str
+}
--- test/main.go
+++ test/main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.Capitalize("hEllo! How arE you? How+are+things+4you? h:"))
-	fmt.Println(piscine.Capitalize("'/O{Q?-{R;%sCY")) // "'/O{Q-{R;%Scy"
-	fmt.Println(piscine.Capitalize("=O3F\\/0-e?<7I")) // "=O3f\\/0-e?<7i"
+	elems := []string{"Hello!", " How", " are", " you?"}
+	fmt.Println(piscine.BasicJoin(elems))
 }
