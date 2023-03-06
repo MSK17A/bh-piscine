73adff48d33e43e984335c59f77056f30faeb244
reboot <student@desk-0121.reboot.internal>
2023-03-06 20:06:35 +0300

    .

---
+++ trimatoi.go
@@ -0,0 +1,33 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	num := 0
+	sign := 1
+	string_rune := []rune(s)
+	string_length := strLen(s)
+	index := 0
+	first_encounter := '0' // A variable to flag what encounterd first (is it a sign or a number)
+	if len(s) > 0 {
+	} else {
+		return 0
+	}
+	// if first encouter is the sign then take it, otherwise don't
+	for i := index; i <= string_length; i++ {
+		if string_rune[i] == 45 && first_encounter == '0' {
+			sign = -1
+			first_encounter = '1'
+		}
+		if string_rune[i] == 43 && first_encounter == '0' {
+			sign = 1
+			first_encounter = '1'
+		}
+		if (string_rune[i] >= 48) && (string_rune[i] <= 57) && (first_encounter == '0') {
+			first_encounter = '1'
+		}
+		// Check if string is a number
+		if (string_rune[i] >= 48) && (string_rune[i] <= 57) {
+			num = num*10 + (int(string_rune[i]) - 48)
+		}
+	}
+	return num * sign
+}
