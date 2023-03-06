d641c09b1641b28195a387ba44ed6c1420ff9359
reboot <student@desk-0121.reboot.internal>
2023-03-06 21:12:07 +0300

    .

--- capitalize.go
+++ capitalize.go
@@ -3,22 +3,21 @@ package piscine
 func Capitalize(s string) string {
 	// If the previous rune contains non alphanumerical characters CAPITALIZE the current rune
 	// Other wise lower the current rune
+	// If the next rune is not alphanumerical (don't do anything)
 
 	s_rune := []rune(s)
 	s_length := strLen(s)
 
 	for i := 0; i <= s_length; i++ {
-		if i > 0 {
-			// if it is not alphanumerical
-			if (s_rune[i-1] < 65 || s_rune[i-1] > 90) && (s_rune[i-1] < 97 || s_rune[i-1] > 122) && (s_rune[i-1] < 48 || s_rune[i-1] > 57) {
-				// If it is lowercase then UPPERCASE IT
-				if s_rune[i] >= 97 && s_rune[i] <= 122 {
-					s_rune[i] -= 32
-				}
-				// If ti is UPPERCASE then lowercase IT
-			} else if s_rune[i] >= 65 && s_rune[i] <= 90 {
-				s_rune[i] += 32
+		// if it is not alphanumerical (the previous rune)
+		if i > 0 && (s_rune[i-1] < 65 || s_rune[i-1] > 90) && (s_rune[i-1] < 97 || s_rune[i-1] > 122) && (s_rune[i-1] < 48 || s_rune[i-1] > 57) {
+			// If it is lowercase then UPPERCASE IT
+			if s_rune[i] >= 97 && s_rune[i] <= 122 {
+				s_rune[i] -= 32
 			}
+		} else if i < s_length && (s_rune[i+1] < 65 || s_rune[i+1] > 90) && (s_rune[i+1] < 97 || s_rune[i+1] > 122) && (s_rune[i+1] < 48 || s_rune[i+1] > 57) {
+			// If it is lowercase then UPPERCASE IT
+			s_rune[i] = rune(s[i])
 		}
 	}
 	return string(s_rune)
---
+++ test/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.Capitalize("hello! How arE you? How+are+things+4you? h:"))
+}
