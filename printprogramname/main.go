590ab24b867093109f52aeb49084d4d7718f4e09
malsamma <msk17a@gmail.com.com>
2023-03-07 09:04:03 +0300

    .

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -7,7 +7,9 @@ import (
 )
 
 func main() {
-	for _, word := range os.Args[0] {
-		z01.PrintRune(word)
+	arg := os.Args
+	for _, w := range os.arg[0] {
+		z01.PrintRune(w)
 	}
+	z01.PrintRune('\n')
 }
