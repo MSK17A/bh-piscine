925a018b329ca2e07381417f71056b85172c0ff9
MSK17A <legendary512@gmail.com>
2023-03-08 18:27:39 +0300

    .

---
+++ splitwhitespaces.go
@@ -0,0 +1,12 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	var strArray []string
+	var str_temp string
+
+	for _, char := range s {
+		if char != '\n' || char != ' ' || char != '	' {
+
+		}
+	}
+}
