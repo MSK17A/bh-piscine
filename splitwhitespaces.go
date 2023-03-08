0ce98d411a7778ed5751627b3227bdf9688e9af7
Your Name <you@example.com>
2023-03-08 19:24:03 +0300

    .

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -2,11 +2,17 @@ package piscine
 
 func SplitWhiteSpaces(s string) []string {
 	var strArray []string
-	var str_temp string
+	var str_temp []rune
 
 	for _, char := range s {
-		if char != '\n' || char != ' ' || char != '	' {
-
+		if char != ' ' && char != 10 && char != 9 {
+			str_temp = append(str_temp, char)
+		} else {
+			strArray = append(strArray, string(str_temp))
+			str_temp = nil
 		}
 	}
+
+	strArray = append(strArray, string(str_temp))
+	return strArray
 }
