488c53aa67de0afd99794224f8483d48bf7d9073
msk17a <msk170a@gmail.com>
2023-03-12 21:20:22 +0300

    .

---
+++ displayfile/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"os"
+)
+
+func main() {
+
+	args := os.Args[1:]
+
+	if len(args) > 1 {
+		fmt.Println("Too many arguments")
+		return
+	}
+	if len(args) < 1 {
+		fmt.Println("File name missing")
+		return
+	}
+
+	file, _ := os.Open("quest8.txt") // For read access.
+
+	buffer := make([]byte, 50)
+	file.Read(buffer)
+
+	fmt.Println(string(buffer))
+
+}
