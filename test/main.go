71bb0f109b3e897750f3dfd1635b7da6dcd9b7ba
MSK17A <legendary512@gmail.com>
2023-03-13 07:12:37 +0300

    .

--- test/main.go
+++ test/main.go
@@ -11,9 +11,11 @@ func main() {
 
 	result1 := piscine.IsSorted(if_first_is_bigger, a1)
 	result2 := piscine.IsSorted(if_first_is_bigger, a2)
+	result3 := piscine.IsSorted(if_first_is_bigger, []int{-655468, -181010, 176221, -330973, -680281, -464101, -68422, 454601})
 
 	fmt.Println(result1)
 	fmt.Println(result2)
+	fmt.Println(result3)
 }
 
 func if_first_is_bigger(first, second int) int {
