b7f986599b95339d6944a40c09482700c149b11f
malsamma <msk17a@gmail.com.com>
2023-03-13 11:18:26 +0300

    .

---
+++ doop/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) > 3 {
+		return
+	}
+	if args[1] != "+" && args[1] != "-" && args[1] != "/" && args[1] != "*" && args[1] != "%" {
+		return
+	}
+
+	a := Atoi(args[0])
+	b := Atoi(args[2])
+	operator := args[1]
+
+	switch operator {
+	case "+":
+		{
+			fmt.Println(a + b)
+		}
+	case "-":
+		{
+			fmt.Println(a - b)
+		}
+	case "/":
+		{
+			fmt.Println(a / b)
+		}
+	case "*":
+		{
+			fmt.Println(a * b)
+		}
+	case "%":
+		{
+			fmt.Println(a % b)
+		}
+
+	}
+
+}
+
+func Atoi(s string) int {
+	num := 0
+	sign := 1
+	string_length := len(s)
+	index := 0
+
+	if len(s) > 0 {
+		if s[0] == 45 {
+			sign = -1
+			index = 1
+		} else if s[0] == 43 {
+			sign = 1
+			index = 1
+		}
+	} else {
+		return 0
+	}
+
+	for i := index; i < string_length; i++ {
+		if (s[i] < 48) || (s[i] > 57) {
+			return 0
+		}
+		num = num*10 + (int(s[i]) - 48)
+	}
+	return num * sign
+}
