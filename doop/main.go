456ca12948bc8cb6a6832b4cb67b706a8e0fe474
malsamma <msk17a@gmail.com.com>
2023-03-16 09:02:08 +0300

    .

--- doop/main.go
+++ doop/main.go
@@ -1,54 +1,85 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
 func main() {
 	args := os.Args[1:]
-
-	if len(args) > 3 {
+	if len(args) != 3 {
 		return
 	}
 	if args[1] != "+" && args[1] != "-" && args[1] != "/" && args[1] != "*" && args[1] != "%" {
 		return
 	}
-
+	for _, char := range args[0] {
+		if (char < '0' || char > '9') && char != '-' {
+			return
+		}
+	}
+	for _, char := range args[2] {
+		if (char < '0' || char > '9') && char != '-' {
+			return
+		}
+	}
 	a := Atoi(args[0])
 	b := Atoi(args[2])
 	operator := args[1]
-
 	switch operator {
 	case "+":
 		{
-			fmt.Println(a + b)
+			if a+b < 9223372036854775807 || a+b < -9223372036854775807 {
+				return
+			}
+			result := a + b
+			PrintNbrStd(result)
+			os.Stdout.WriteString("\n")
 		}
 	case "-":
 		{
-			fmt.Println(a - b)
+			if a-b < 9223372036854775807 || a-b < -9223372036854775807 {
+				return
+			}
+			result := a - b
+			PrintNbrStd(result)
+			os.Stdout.WriteString("\n")
 		}
 	case "/":
 		{
-			fmt.Println(a / b)
+			if b == 0 {
+				os.Stdout.WriteString("No division by 0\n")
+				return
+			}
+			result := a / b
+			PrintNbrStd(result)
+			os.Stdout.WriteString("\n")
 		}
 	case "*":
 		{
-			fmt.Println(a * b)
+			if a*b < 9223372036854775807 || a*b < -9223372036854775807 {
+				return
+			}
+			result := a * b
+			PrintNbrStd(result)
+			os.Stdout.WriteString("\n")
 		}
 	case "%":
 		{
-			fmt.Println(a % b)
+			if b == 0 {
+				os.Stdout.WriteString("No modulo by 0\n")
+				return
+			}
+			result := a % b
+			PrintNbrStd(result)
+			os.Stdout.WriteString("\n")
 		}
 	}
 }
-
 func Atoi(s string) int {
 	num := 0
 	sign := 1
 	string_length := len(s)
 	index := 0
-
 	if len(s) > 0 {
 		if s[0] == 45 {
 			sign = -1
@@ -60,7 +91,6 @@ func Atoi(s string) int {
 	} else {
 		return 0
 	}
-
 	for i := index; i < string_length; i++ {
 		if (s[i] < 48) || (s[i] > 57) {
 			return 0
@@ -68,4 +98,59 @@ func Atoi(s string) int {
 		num = num*10 + (int(s[i]) - 48)
 	}
 	return num * sign
+}
+func conv_to_ASCII(num int, sign int) string {
+	if num == 0 {
+		return "0"
+	}
+	digit := int((num % 10) * sign)
+	_num := int(num / 10)
+	// z01.PrintRune(48 + rune(digit))
+	return conv_to_ASCII(_num, sign) + string(48+digit)
+}
+func PrintNbrStd(n int) {
+	sign := 1
+	if n == 0 {
+		os.Stdout.WriteString("0")
+		return
+	}
+	if n < 0 {
+		sign *= -1
+		os.Stdout.WriteString("-")
+	}
+	os.Stdout.WriteString(conv_to_ASCII(n, sign)[1:])
+}
+func AtoiOverflow(a, b, c int64) bool {
+	if a < 0 && c < 0 {
+		return a*b+c < 0
+	} else if a > 0 && c > 0 {
+		return a*b+c > 0
+	}
+	return true
+}
+func MultiplyOverflow(a, b, c int64) bool {
+	prod := a*b + c
+	return (prod/b)-c == a
+}
+func PlusOverflow(a, b int64) bool {
+	if a < 0 && b < 0 {
+		return a+b < 0
+	} else if a > 0 && b > 0 {
+		return a+b > 0
+	}
+	return true
+}
+func MinusOverflow(a, b int64) bool {
+	if a < 0 && b < 0 {
+		if b <= a {
+			return a-b >= 0
+		}
+		return a-b < 0
+	} else if a > 0 && b > 0 {
+		if a <= b {
+			return a-b <= 0
+		}
+		return a-b > 0
+	}
+	return true
 }
