15eb18195239b7c749a98a371310ba0f870fcfb9
MSK17A <legendary512@gmail.com>
2023-03-22 01:11:51 +0300

    .

--- listmerge.go
+++ listmerge.go
@@ -16,6 +16,6 @@ func ListMerge(l1 *List, l2 *List) {
 	if l1.Tail != nil && l2.Head != nil {
 		l1.Tail.Next = l2.Head
 	} else if l2.Head != nil && l1.Tail == nil {
-		l1.Tail = l2.Head
+		l1.Head = l2.Head
 	}
 }
