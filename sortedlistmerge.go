609fbaff09fef547476c0fbf0abef180f4f69171
MSK17A <legendary512@gmail.com>
2023-03-22 02:57:39 +0300

    .

---
+++ sortedlistmerge.go
@@ -0,0 +1,35 @@
+package piscine
+
+func SortedListMerge(n1 *NodeI, n2 *NodeI) *NodeI {
+	var head, tail *NodeI
+
+	// set the head and tail to the smaller node
+	if n1.Data < n2.Data {
+		head, tail = n1, n1
+		n1 = n1.Next
+	} else {
+		head, tail = n2, n2
+		n2 = n2.Next
+	}
+
+	// merge the two lists
+	for n1 != nil && n2 != nil {
+		if n1.Data < n2.Data {
+			tail.Next = n1
+			n1 = n1.Next
+		} else {
+			tail.Next = n2
+			n2 = n2.Next
+		}
+		tail = tail.Next
+	}
+
+	// append the remaining nodes
+	if n1 != nil {
+		tail.Next = n1
+	} else if n2 != nil {
+		tail.Next = n2
+	}
+
+	return head
+}
