cf0c3783cf0c2fb9d2fc9cfffc6ad1b5fd9caa1c
MSK17A <legendary512@gmail.com>
2023-03-22 02:50:23 +0300

    .

---
+++ sortlistinsert.go
@@ -0,0 +1,21 @@
+package piscine
+
+func SortListInsert(l *NodeI, data_ref int) *NodeI {
+
+	newNode := &NodeI{Data: data_ref}
+
+	if l == nil || data_ref < l.Data {
+		newNode.Next = l
+		return newNode
+	}
+
+	curr := l
+	for curr.Next != nil && data_ref > curr.Next.Data {
+		curr = curr.Next
+	}
+
+	newNode.Next = curr.Next
+	curr.Next = newNode
+
+	return l
+}
