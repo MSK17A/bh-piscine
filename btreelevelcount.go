fe2ddf00372f12a94dd3da5631815e5529eb38a5
MSK17A <legendary512@gmail.com>
2023-03-22 19:38:44 +0300

    .

--- btreelevelcount.go
+++ btreelevelcount.go
@@ -5,6 +5,15 @@ func BTreeLevelCount(root *TreeNode) int {
 		return 0
 	}
 
-	BTreeLevelCount(root.Left)
-	return BTreeLevelCount(root.Right) + 1
+	left_height := BTreeLevelCount(root.Left)
+	right_height := BTreeLevelCount(root.Right)
+
+	return max(left_height, right_height) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
