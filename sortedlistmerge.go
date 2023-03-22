efbe885a6ad6c2e5f01c06df4b3e3e2a16fa0038
MSK17A <legendary512@gmail.com>
2023-03-22 03:00:45 +0300

    .

--- sortedlistmerge.go
+++ sortedlistmerge.go
@@ -2,6 +2,17 @@ package piscine
 
 func SortedListMerge(n1 *NodeI, n2 *NodeI) *NodeI {
 	var head, tail *NodeI
+	if n1 == nil && n2 == nil {
+		return nil
+	}
+
+	if n2 == nil {
+		return n1
+	}
+
+	if n1 == nil {
+		return n2
+	}
 
 	// set the head and tail to the smaller node
 	if n1.Data < n2.Data {
