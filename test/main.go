a68896274f32089beb57b1f56e7d32c2357e4f5d
MSK17A <legendary512@gmail.com>
2023-03-22 19:16:44 +0300

    .

--- test/main.go
+++ test/main.go
@@ -9,6 +9,6 @@ func main() {
 	root := &piscine.TreeNode{Data: "04"}
 	piscine.BTreeInsertData(root, "07")
 	piscine.BTreeInsertData(root, "01")
-	//piscine.BTreeInsertData(root, "5")
+	// piscine.BTreeInsertData(root, "5")
 	fmt.Println(piscine.BTreeLevelCount(root) + 1)
 }
