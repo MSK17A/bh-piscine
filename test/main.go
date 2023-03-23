95d618a27cb75cacfdbfa9474314365e58415345
Mohammed AlSammak <msk170a@gmail.com>
2023-03-24 00:57:28 +0300

    .

--- test/main.go
+++ test/main.go
@@ -10,6 +10,6 @@ func main() {
 	piscine.BTreeInsertData(root, "1")
 	piscine.BTreeInsertData(root, "7")
 	piscine.BTreeInsertData(root, "5")
-	max := piscine.BTreeMax(root)
+	max := piscine.BTreeMin(root)
 	fmt.Println(max.Data)
 }
