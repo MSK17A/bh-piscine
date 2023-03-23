9c84015f11a814a8fc21bc70c9bde16737bb2d5a
MSK17A <legendary512@gmail.com>
2023-03-24 01:54:22 +0300

    .

--- btreetransplant.go
+++ btreetransplant.go
@@ -4,4 +4,15 @@ func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
+	if root.Left == node && root.Left != nil {
+		rplc.Parent = root
+		root.Left = rplc
+	} else if root.Right == node && root.Right != nil {
+		rplc.Parent = root
+		root.Right = rplc
+	} else {
+		BTreeTransplant(root.Left, node, rplc)
+		BTreeTransplant(root.Right, node, rplc)
+	}
+	return root
 }
